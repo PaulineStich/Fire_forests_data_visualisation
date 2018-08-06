6bb9880ee7bab4ac61d674beb9e8c3ac10a1fa6b
popo <miss.poepoe23@gmail.com>
2018-08-06 11:47:28 +0200

    :wrench: build: webpack config image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 let webpack = require('webpack');
 let path = require('path');
+let glob = require('glob');
 let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 let MinifyPlugin = require("babel-minify-webpack-plugin");
 let MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+let PurifyCSSPlugin = require('purifycss-webpack');
 
 const prodMode = (process.env.NODE_ENV === 'production');
 
@@ -32,12 +34,35 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|pdf|woff|woff2|svg)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             name: '[path][name].[ext]',
+                        }
+                    },
+                    {
+                        //optimize img size
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
                         }
                     }
                 ]
@@ -58,6 +83,10 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css',
+        }),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'index.html')),
+            minimize: prodMode
         })
     ]
 };
